994134bff8e09591c450ce1821d096c168567208
Christopher Cortez <113377113+cec5@users.noreply.github.com>
2025-05-09 16:09:36 -0400

    really basic homepage

--- frontend/index.php
+++ frontend/index.php
@@ -1,1 +1,11 @@
-
+<?php include(__DIR__.'/common/header.php');?>
+<main class="container my-5">
+    	<!-- Content Section -->
+    	<div class="mx-auto px-auto my-5 py-5 text-center">
+        	<h1 class="display-4 fw-bold">Dancing Birds</h1>
+        	<div class="col-lg-10 mx-auto">
+        		<p class="fs-3 lead mb-3">Final Project for NJIT's BIOL 115 - Evolution and Biology of Sex</p>
+        	</div>
+    	</div>
+</main>
+<?php include(__DIR__.'/common/footer.php');?>
