a6b9c0680bfbf6faf913acd6315f8caaf2bd391d
Christopher Cortez <113377113+cec5@users.noreply.github.com>
2025-05-13 13:00:21 -0400

    added simple database/table

--- backend/database/tables.sql
+++ backend/database/tables.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE IF NOT EXISTS birds (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        scientific_name VARCHAR(150) NOT NULL,
+        habitat VARCHAR(255),
+        description TEXT,
+        image_url VARCHAR(255),
+        video_url VARCHAR(255),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
